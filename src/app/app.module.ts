bc539fb2fd6691364f961c13f7e0be7998ac116e
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-04-12 15:48:33 -0300

    chore(module): adicao de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeModule } from './pages/home/home.module';
+import { ProductViewComponent } from './pages/home/product-view/product-view.component';
+import { ProductListComponent } from './pages/home/product-list/product-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProductEditComponent } from './pages/home/product-edit/product-edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
