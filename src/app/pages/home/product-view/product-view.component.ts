ca4090ad3cadf2a1d8a8006eb7e23e67c8e75870
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-04-16 09:47:49 -0300

    refactor(product): ajustes de identação

--- src/app/pages/home/product-view/product-view.component.ts
+++ src/app/pages/home/product-view/product-view.component.ts
@@ -4,14 +4,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
-  styleUrls: ['./product-view.component.css']
+  styleUrls: ['./product-view.component.css'],
 })
 export class ProductViewComponent {
   @Input() productItem?: Product;
 
-@Output() edit = new EventEmitter<number>();
-@Output() list = new EventEmitter<void>();
-ngOnInit(){
-
-}
+  @Output() edit = new EventEmitter<number>();
+  @Output() list = new EventEmitter<void>();
+  ngOnInit() {}
 }
