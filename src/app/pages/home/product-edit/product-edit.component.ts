d1f909150d7dc9d6e8d47f7bcc58fbf9552339c0
Hamilton <hamilton.rodrigues.teixeira@gmail.com>
2025-04-16 09:49:50 -0300

    feat(product-edit): implementacao do metodo de salvar novo produto e produto editado

--- src/app/pages/home/product-edit/product-edit.component.ts
+++ src/app/pages/home/product-edit/product-edit.component.ts
@@ -1,47 +1,55 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Product } from 'src/app/domain/models/product';
+import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
-  styleUrls: ['./product-edit.component.css']
+  styleUrls: ['./product-edit.component.css'],
 })
 export class ProductEditComponent {
   @Input() productItem?: Product;
 
-  @Output() save = new EventEmitter<void>();
+  @Output() save = new EventEmitter<Product>();
   @Output() cancel = new EventEmitter<void>();
 
-productForm = new FormGroup ({
-  nome: new FormControl("",[Validators.required]),
-  descricao: new FormControl("",[Validators.required]),
-  preco: new FormControl(0,[Validators.required]),
-})
+  productForm = new FormGroup({
+    id: new FormControl(0, [Validators.required]),
+    nome: new FormControl('', [Validators.required]),
+    descricao: new FormControl('', [Validators.required]),
+    preco: new FormControl(0, [Validators.required]),
+  });
 
-ngOnInit(){
-  this.updateForm()
-  // this.productForm.get('nome')?.setValue(this.productItem?.nome || '')
-  // this.productForm.get('descricao')?.setValue(this.productItem?.descricao || '')
-  // this.productForm.get('preco')?.setValue(this.productItem?.preco || 0)
-}
+  constructor() {}
+  ngOnInit() {
+    this.updateForm();
 
+    ///Forma de inicializar os valores nos INPUTS diretamente
+    // this.productForm.get('nome')?.setValue(this.productItem?.nome || '')
+    // this.productForm.get('descricao')?.setValue(this.productItem?.descricao || '')
+    // this.productForm.get('preco')?.setValue(this.productItem?.preco || 0)
+  }
 
-updateForm(){
-  // this.productForm.patchValue({
-  //   nome: this.productItem?.nome,
-  //   descricao: this.productItem?.descricao,
-  //   preco: this.productItem?.preco
-  // });
-  this.productForm.patchValue(this.productItem || {})
-  // this.productForm.patchValue({
-  //   ...this.productItem,
-  // })
-}
+  updateForm() {
+    this.productForm.patchValue(this.productItem || {});
 
-onSubmit() {
-  console.warn(this.productForm.value);
-}
+    ////FORMAS DIFERENTES DE INICIAR OS VALORES NOS INPUTS
 
+    // this.productForm.patchValue({
+    //   nome: this.productItem?.nome,
+    //   descricao: this.productItem?.descricao,
+    //   preco: this.productItem?.preco
+    // });
+    // this.productForm.patchValue({
+    //   ...this.productItem,
+    // })
+  }
 
+  onSubmit() {
+    console.log(this.productForm.value);
+    if (this.productForm.valid) {
+    }
+    this.save.emit(this.productForm.value as Product);
+  }
 }
